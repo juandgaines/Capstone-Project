0d80067383f4f85401c17b0e6402cf810f47129e
Juan Gaines <jdavid90.2@gmail.com>
2019-03-28 22:11:53 -0500

    feat:Splash activityV2
    Same commit the las one was an error
    
    Resolves:NA
    See also:NA

--- app/src/main/java/com/mytechideas/bodytracker/MainActivity.java
+++ app/src/main/java/com/mytechideas/bodytracker/MainActivity.java
@@ -1,7 +1,8 @@
 package com.mytechideas.bodytracker;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
