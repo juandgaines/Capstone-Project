44ae662dfbde4a6f2fb5f036c12fdc5cef91d3bb
Juan Gaines <jdavid90.2@gmail.com>
2019-04-20 17:39:00 -0500

    feat: UPC- reader working
    Reader working with firebase
    
    Resolves:NA
    See also:NA

--- app/src/main/java/com/mytechideas/bodytracker/mainviewpager/BarCodeScannerActivity.java
+++ app/src/main/java/com/mytechideas/bodytracker/mainviewpager/BarCodeScannerActivity.java
@@ -1,5 +1,6 @@
 package com.mytechideas.bodytracker.mainviewpager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.exifinterface.media.ExifInterface;
 import butterknife.BindView;
@@ -10,19 +11,35 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.mytechideas.bodytracker.R;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class BarCodeScannerActivity extends AppCompatActivity {
 
     @BindView(R.id.preview_image)
     ImageView mPreviewImage;
+    @BindView(R.id.serial)
+    TextView mPreviwSerial;
 
 
     @Override
@@ -31,6 +48,15 @@ public class BarCodeScannerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_bar_code_scanner);
         ButterKnife.bind(this);
         Intent intent=getIntent();
+
+        FirebaseVisionBarcodeDetectorOptions options =
+                new FirebaseVisionBarcodeDetectorOptions.Builder()
+                        .setBarcodeFormats(
+                                FirebaseVisionBarcode.FORMAT_UPC_A,
+                                FirebaseVisionBarcode.FORMAT_UPC_E)
+                        .build();
+
+
         if(intent!=null && intent.hasExtra("image")){
 
 
@@ -58,7 +84,50 @@ public class BarCodeScannerActivity extends AppCompatActivity {
 
             mPreviewImage.setImageBitmap(rotatedBitmap);
 
+            FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(rotatedBitmap);
 
+            FirebaseVisionBarcodeDetector detector = FirebaseVision.getInstance()
+                    .getVisionBarcodeDetector();
+
+
+            Task<List<FirebaseVisionBarcode>> result = detector.detectInImage(image)
+                    .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
+                        @Override
+                        public void onSuccess(List<FirebaseVisionBarcode> barcodes) {
+                            // Task completed successfully
+
+                            for (FirebaseVisionBarcode barcode: barcodes) {
+                                Rect bounds = barcode.getBoundingBox();
+                                Point[] corners = barcode.getCornerPoints();
+
+                                String rawValue = barcode.getRawValue();
+
+                                mPreviwSerial.setText(rawValue);
+
+                                int formatType=barcode.getFormat();
+
+                                int valueType = barcode.getValueType();
+                                // See API reference for complete list of supported types
+                                switch (formatType) {
+                                    case FirebaseVisionBarcode.FORMAT_UPC_A:
+
+                                        Toast.makeText(BarCodeScannerActivity.this,"Type UPC-A",Toast.LENGTH_LONG).show();
+                                        break;
+                                    case FirebaseVisionBarcode.FORMAT_UPC_E:
+                                        Toast.makeText(BarCodeScannerActivity.this,"Type UPC-E",Toast.LENGTH_LONG).show();
+                                        break;
+                                }
+                            }
+                            // ...
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            // Task failed with an exception
+                            // ...
+                        }
+                    });
 
         }
     }
